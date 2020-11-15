21023270830c0604ff8db3d166cca7d0132698ac
alejomontoya <montoyaa241@gmail.com>
2020-11-14 21:43:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+require('dotenv').config()
+
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
+
+// Define initial schema
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+app.use(cors())
+
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: isDev
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
